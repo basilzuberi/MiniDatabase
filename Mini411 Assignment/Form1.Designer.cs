92aa72773afb94537bcae1a7cb92732334d9837e
Basil <basilzuberi@hotmail.com>
2019-01-17 21:49:50 -0500

    Add project files.

---
+++ Mini411 Assignment/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace Mini411_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearchNumber = new System.Windows.Forms.Button();
+            this.btnSearchName = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.lbNames = new System.Windows.Forms.ListBox();
+            this.lbPhone = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.lblCustomers = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(92, 24);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(92, 55);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 20);
+            this.txtPhone.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(5, 84);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Phone Number";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSearchNumber);
+            this.groupBox1.Controls.Add(this.btnSearchName);
+            this.groupBox1.Controls.Add(this.btnDelete);
+            this.groupBox1.Controls.Add(this.btnEdit);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAdd);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPhone);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 157);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add Information";
+            // 
+            // btnSearchNumber
+            // 
+            this.btnSearchNumber.Location = new System.Drawing.Point(130, 113);
+            this.btnSearchNumber.Name = "btnSearchNumber";
+            this.btnSearchNumber.Size = new System.Drawing.Size(101, 23);
+            this.btnSearchNumber.TabIndex = 8;
+            this.btnSearchNumber.Text = "Search by #";
+            this.btnSearchNumber.UseVisualStyleBackColor = true;
+            this.btnSearchNumber.Click += new System.EventHandler(this.btnSearchNumber_Click);
+            // 
+            // btnSearchName
+            // 
+            this.btnSearchName.Location = new System.Drawing.Point(29, 113);
+            this.btnSearchName.Name = "btnSearchName";
+            this.btnSearchName.Size = new System.Drawing.Size(94, 23);
+            this.btnSearchName.TabIndex = 7;
+            this.btnSearchName.Text = "Search by Name";
+            this.btnSearchName.UseVisualStyleBackColor = true;
+            this.btnSearchName.Click += new System.EventHandler(this.btnSearchName_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(175, 84);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(88, 84);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(79, 23);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit Phone #";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // lbNames
+            // 
+            this.lbNames.FormattingEnabled = true;
+            this.lbNames.Location = new System.Drawing.Point(15, 245);
+            this.lbNames.Name = "lbNames";
+            this.lbNames.Size = new System.Drawing.Size(120, 121);
+            this.lbNames.TabIndex = 11;
+            this.lbNames.SelectedIndexChanged += new System.EventHandler(this.lbNames_SelectedIndexChanged);
+            // 
+            // lbPhone
+            // 
+            this.lbPhone.FormattingEnabled = true;
+            this.lbPhone.Location = new System.Drawing.Point(141, 245);
+            this.lbPhone.Name = "lbPhone";
+            this.lbPhone.Size = new System.Drawing.Size(120, 121);
+            this.lbPhone.TabIndex = 12;
+            this.lbPhone.SelectedIndexChanged += new System.EventHandler(this.lbPhone_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Names:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(156, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Phone Numbers:";
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(104, 201);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(75, 23);
+            this.btnShowAll.TabIndex = 10;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // lblCustomers
+            // 
+            this.lblCustomers.AutoSize = true;
+            this.lblCustomers.Location = new System.Drawing.Point(12, 172);
+            this.lblCustomers.Name = "lblCustomers";
+            this.lblCustomers.Size = new System.Drawing.Size(120, 13);
+            this.lblCustomers.TabIndex = 13;
+            this.lblCustomers.Text = "Number of Customers: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 380);
+            this.Controls.Add(this.lblCustomers);
+            this.Controls.Add(this.btnShowAll);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbPhone);
+            this.Controls.Add(this.lbNames);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Mini 411";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.ListBox lbNames;
+        private System.Windows.Forms.ListBox lbPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSearchNumber;
+        private System.Windows.Forms.Button btnSearchName;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Label lblCustomers;
+    }
+}
+
---
+++ Mini411 Assignment/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mini411_Assignment
+{
+    public partial class Form1 : Form
+    {
+        string[] NameArray = new string[100];  //Declare global variables
+        string[] PhoneArray = new string[100];
+        int iCurrentIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strName, strPhone;
+
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+
+                strName = txtName.Text.Trim();
+                strPhone = txtPhone.Text.Trim();
+
+                if(string.IsNullOrWhiteSpace(txtPhone.Text) || string.IsNullOrWhiteSpace(txtName.Text)) //check to see if the textboxes are blank
+                {
+                    MessageBox.Show("Please fill out all values");
+                    txtName.Clear();
+                    txtPhone.Clear();
+                    return;
+                }
+                    if (iCurrentIndex < 100) //checks the array's index
+                    {
+                        NameArray[iCurrentIndex] = strName; //adds name in the current index of the array
+                        PhoneArray[iCurrentIndex] = strPhone;//adds phone number in the current index of the array
+                        iCurrentIndex++;
+                    }
+                    else // if index value exceeds the size of the array
+                    {
+                        MessageBox.Show("No more space! Please delete someone to add more.");
+                    }
+                    MessageBox.Show("Successfully added");
+                    lblCustomers.Text = "Number of Customers: " + iCurrentIndex; //if user has been added outputs the number of customers
+                    txtName.Clear();
+                    txtPhone.Clear();
+                }
+            catch
+            {
+                MessageBox.Show("Enter valid information!");
+                txtName.Clear();
+                txtPhone.Clear();
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            int i;
+
+            lbNames.Items.Clear();
+            lbPhone.Items.Clear();
+
+            for (i = 0; i < iCurrentIndex; i++) //loop to display names and phone numbers
+            {
+                lbNames.Items.Add(NameArray[i]); //outputs names to lbnames
+                lbPhone.Items.Add(PhoneArray[i]); //outputs phone numbers to lbphone
+            }
+        }
+
+        private void btnSearchName_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i, counter;
+                string strName;
+
+                strName = txtName.Text.Trim().ToLower();
+
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+
+                counter = 0;
+
+                for (i = 0; i < iCurrentIndex; i++) //loop to check if the 411 has the name entered
+                {
+                    if (NameArray[i].Trim().ToLower() == strName) //if the entered name is available in the array
+                    {
+                        lbNames.Items.Add(NameArray[i]); //displays the name of searched person
+                        lbPhone.Items.Add(PhoneArray[i]);//displays the number of searched person
+                        counter++;
+                    }
+                }
+                if (counter == 0) //if the person does not exist, shows an error
+                {
+                    MessageBox.Show("Name does not exist in 411!");
+                    txtName.Clear();
+                    txtPhone.Clear();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Enter valid information!");
+                txtName.Clear();
+                txtPhone.Clear();
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+            }
+        }
+
+        private void btnSearchNumber_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i, counter;
+                string strPhone;
+
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+
+                strPhone = txtPhone.Text.Trim();
+
+                counter = 0;
+
+                for (i = 0; i < iCurrentIndex; i++) //loop to check if the 411 has the phone number entered 
+                {
+                    if (PhoneArray[i].Trim().ToLower() == strPhone) //if the entered phone number is available in the array
+                    {
+                        lbNames.Items.Add(NameArray[i]); //displays the name of the person with the entered phone number
+                        lbPhone.Items.Add(PhoneArray[i]); //displays the phone number in of the person
+                        counter++;
+                    }
+                }
+                if (counter == 0) //if the phone number does not exist, shows an error
+                {
+                    MessageBox.Show("Number does not exist in 411!");
+                    txtName.Clear();
+                    txtPhone.Clear();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Enter valid information!");
+                txtName.Clear();
+                txtPhone.Clear();
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            int i, iCounter;
+            string strName, strPhone;
+            
+            lbNames.Items.Clear();
+            lbPhone.Items.Clear();
+
+            strName = txtName.Text.Trim().ToLower();
+            strPhone = txtPhone.Text.Trim().ToLower();
+
+            iCounter = 0;
+
+            if (string.IsNullOrWhiteSpace(txtPhone.Text) || string.IsNullOrWhiteSpace(txtName.Text)) //check to see if the textboxes are blank
+            {
+                MessageBox.Show("Please fill out all values");
+                return;
+            }
+            for (i = 0; i < iCurrentIndex; i++)
+            {
+                if (NameArray[i].Trim().ToLower() == strName) //checks the name to edit
+                {
+                    PhoneArray[i] = strPhone; // replaces the current phone number with the entered phone number
+                    iCounter++;
+                    MessageBox.Show("Sucessfully Edited");
+                    txtName.Clear();
+                    txtPhone.Clear();
+                }
+            }
+            if (iCounter == 0)//if person doesn't exist, shows error.
+            {
+                MessageBox.Show("This person does not exist in 411!");
+                txtName.Clear();
+                txtPhone.Clear();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i, j, counter;
+                string strName, strPhone;
+
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+
+                strName = txtName.Text.Trim().ToLower();
+                strPhone = txtPhone.Text.Trim().ToLower();
+
+                counter = 0;
+
+                if (string.IsNullOrWhiteSpace(txtPhone.Text) || string.IsNullOrWhiteSpace(txtName.Text))
+                {
+                    MessageBox.Show("Please fill out all values");
+                    txtName.Clear();
+                    txtPhone.Clear();
+                    return;
+                }
+                else
+                {
+                    for (i = 0; i < iCurrentIndex; i++)
+                    {
+                        if (NameArray[i].Trim().ToLower() == strName && PhoneArray[i].Trim().ToLower() == strPhone) //checks if user selected a name or phone number
+                        {
+                            if (i == 99)
+                            {
+                                iCurrentIndex = iCurrentIndex - 1; //removes the name needed to be deleted
+                                MessageBox.Show("Successfully Deleted!");//sucessfully deleted
+                                txtName.Clear();
+                                txtPhone.Clear();
+                                return;
+                            }
+                            else
+                            {
+                                for (j = i; j < iCurrentIndex; j++)
+                                {
+                                    NameArray[j] = NameArray[j + 1]; //moves the names up the list
+                                    PhoneArray[j] = PhoneArray[j + 1]; //moves the phone numbers up the list
+                                    iCurrentIndex = iCurrentIndex - 1; //removes the name needed to be deleted
+                                    lblCustomers.Text = "Number of Customers: " + iCurrentIndex.ToString(); //subtracts the total number of customers
+                                    MessageBox.Show("Successfully Deleted!");//sucessfully deleted
+                                    txtName.Clear();
+                                    txtPhone.Clear();
+                                    return;
+                                }
+                            }
+                            counter = counter++;
+                        }
+                    }
+                    if (counter == 0)
+                    {
+                        MessageBox.Show("Delete failed: Wrong number or Name");
+                        txtName.Clear();
+                        txtPhone.Clear();
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Enter valid information!");
+                txtName.Clear();
+                txtPhone.Clear();
+                lbNames.Items.Clear();
+                lbPhone.Items.Clear();
+            }
+        }
+
+        private void lbNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index;
+            index = lbNames.SelectedIndex;
+            lbPhone.SelectedIndex = index;
+        }
+
+        private void lbPhone_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index;
+            index = lbPhone.SelectedIndex;
+            lbNames.SelectedIndex = index;
+        }
+    }
+}
